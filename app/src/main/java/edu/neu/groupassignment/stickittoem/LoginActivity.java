70057e55356983b8bd47797b184e6211d1245102
wangzechen <wishacoop@gmail.com>
2021-03-18 22:23:01 -0400

    Connect to Realtime Database

--- app/src/main/java/edu/neu/groupassignment/stickittoem/LoginActivity.java
+++ app/src/main/java/edu/neu/groupassignment/stickittoem/LoginActivity.java
@@ -3,12 +3,32 @@ package edu.neu.groupassignment.stickittoem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    private DatabaseReference userRef;
+    private TextView username;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.etUsername);
+        login = findViewById(R.id.btnLogin);
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userRef.child("" + username.getText()).setValue("");
+            }
+        });
     }
 }
