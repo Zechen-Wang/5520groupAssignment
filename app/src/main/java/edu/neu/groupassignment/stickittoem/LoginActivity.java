15120fb1f64c744c1e08aaf97d5a36038afe8497
yutinggan <gan.yuti@gmail.com>
2021-03-19 10:41:22 -0400

    emoji page

--- app/src/main/java/edu/neu/groupassignment/stickittoem/LoginActivity.java
+++ app/src/main/java/edu/neu/groupassignment/stickittoem/LoginActivity.java
@@ -1,11 +1,11 @@
 package edu.neu.groupassignment.stickittoem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
