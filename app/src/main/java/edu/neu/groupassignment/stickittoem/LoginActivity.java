6b3d9bb12a2fe775cec4341148980e875569bb73
KevinSonl <huang.weiq@northeastern.edu>
2021-03-21 03:30:29 -0400

    login to create User object and save in database

--- app/src/main/java/edu/neu/groupassignment/stickittoem/LoginActivity.java
+++ app/src/main/java/edu/neu/groupassignment/stickittoem/LoginActivity.java
@@ -12,30 +12,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import edu.neu.groupassignment.stickittoem.fcm.FCMActivity;
+
 public class LoginActivity extends AppCompatActivity {
+
     private DatabaseReference userRef;
-    private TextView username;
-    private Button login;
+    private TextView usernameTextView;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = findViewById(R.id.etUsername);
-        login = findViewById(R.id.btnLogin);
+        usernameTextView = findViewById(R.id.usernameTextView);
+        loginBtn = findViewById(R.id.loginBtn);
         userRef = FirebaseDatabase.getInstance().getReference().child("users");
-
 
     }
 
     public void loginToMainUI(View v) {
-        if (username.getText().length() == 0) {
+        if (usernameTextView.getText().length() == 0) {
             Toast.makeText(this, "PLease enter a username", Toast.LENGTH_LONG).show();
             return;
         }
-        userRef.child("" + username.getText()).setValue("");
-        Intent intent = new Intent(this, MainActivity.class);
+        String username = usernameTextView.getText().toString();
+        Intent intent = new Intent(this, FCMActivity.class);
+        intent.putExtra("username", username);
         startActivity(intent);
     }
 
